c0042c272cf995666782810277728990a7efb978
stepanskyvlad <vlad14yarema@gmail.com>
2025-02-09 00:14:05 +0200

    finished project1

---
+++ Project1/books.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+BOOKS: list[dict[str, str]] = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def read_all_books() -> list[dict[str, str]]:
+    return BOOKS
+
+# Path dynamic parameters
+@app.get("/books/{book_title}")
+async def read_book(book_title: str) -> dict[str, str]:
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+
+# Query parameters
+@app.get("/books/")
+async def read_category_by_query(category: str) -> list[dict[str, str]]:
+    books_to_return: list[dict[str, str]] = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+# Path and query parameters
+@app.get("/books/{author}/")
+async def read_author_category_by_query(author: str, category: str) -> list[dict[str, str]]:
+    books_to_return: list[dict[str, str]] = []
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold() and \
+                book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get("/books/by_author/{author}")
+async def read_author_books(author: str) -> list[dict[str, str]]:
+    books_to_return: list[dict[str, str]] = []
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(updated_book: dict[str, str]=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
